3fecd2adb3acd72bbcaea71666599f3029453856
omeymaa <omeyma.maatoug@gmail.com>
2023-05-15 21:38:20 +0200

    ajout intro et presentation

--- index.php
+++ index.php
@@ -17,12 +17,41 @@ require('templates/header.php');
     </section>
 
     <div class="scroll text-center p-5">
-        <a class="scroll-down purple" href="#"><i class="fa-solid fa-circle-chevron-down fa-2xl"></i></a>
+        <a class="scroll-down purple" href="#introduction"><i class="fa-solid fa-circle-chevron-down fa-2xl"></i></a>
     </div>
 </div>
 
 <!-- Presentation -->
+<section class="container py-5" id="introduction">
+    <h2>Découvrez l’authenticité de Quai Antique</h2>
+    <p>Hac habitasse platea dictumst quisque sagittis purus sit amet volutpat consequat mauris nunc congue nisi vitae suscipit tellus mauris a diam maecenas sed enim ut sem viverra aliquet eget sit amet tellus cras adipiscing enim eu turpis egestas pretium aenean pharetra magna ac placerat vestibulum lectus mauris ultrices eros in cursus turpis massa tincidunt dui ut ornare <strong>lectus sit amet est placerat in egestas erat imperdiet sed euismod</strong> nisi porta lorem mollis aliquam ut porttitor leo a diam sollicitudin tempor id eu nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit amet risus nullam eget felis eget nunc lobortis mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce id velit ut tortor pretium viverra suspendisse potenti nullam ac tortor vitae.</p>
+</section>
 
+<section class="container py-5" id="presentation">
+    <div class="row pb-5 align-items-md-stretch d-flex justify-content-center">
+        <div class="image-presentation col-lg-6 col-12">
+            <img class="img-fluid" src="assets/images/restaurant-quai-antique.jpg" alt="Presentation du restaurant">
+        </div>
+
+        <div class="text-presentation col-lg-6 col-12 align-self-center pt-2">
+            <h2>Quai antique, typiquement savoyard</h2>
+            <p>Pretium nibh ipsum consequat nisl vel pretium lectus quam id leo in vitae turpis massa sed elementum tempus egestas sed sed risus pretium quam vulputate dignissim suspendisse in est ante in nibh mauris cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit amet nulla facilisi morbi tempus iaculis urna id volutpat lacus laoreet non curabitur gravida arcu ac <strong>tortor dignissim convallis aenean et tortor at risus viverra adipiscing at in tellus integer feugiat scelerisque</strong> varius morbi enim nunc faucibus a pellentesque sit amet porttitor eget dolor morbi non arcu risus quis varius quam quisque id diam vel quam elementum.</p>
+            <a href="about.php" class="orange-button btn text-uppercase">En savoir plus</a>
+        </div>
+    </div>
+
+    <div class="row mt-5 align-items-md-stretch d-flex justify-content-center">
+        <div class="text-presentation col-lg-6 col-12 align-self-center pt-2">
+            <h2>Cuisine artisanale & produits de qualité</h2>
+            <p>Pretium nibh ipsum consequat nisl vel pretium lectus quam id leo in vitae turpis <strong>massa sed elementum tempus egestas sed sed risus pretium quam vulputate</strong> dignissim suspendisse in est ante in nibh mauris cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit amet nulla facilisi morbi tempus iaculis urna id volutpat lacus laoreet non curabitur gravida arcu ac tortor dignissim convallis aenean et tortor at risus viverra adipiscing at in tellus integer feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit amet porttitor eget dolor morbi non arcu risus quis varius quam quisque id diam vel quam elementum.</p>
+            <a href="menu.php" class="orange-button btn text-uppercase">Voir la carte</a>
+        </div>
+
+        <div class="image-presentation col-lg-6 col-12">
+            <img class="img-fluid" src="assets/images/menu-quai-antique.jpg" alt="Presentation Menus">
+        </div>
+    </div>
+</section>
 
 <!-- Galerie -->
 
